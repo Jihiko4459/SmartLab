eb84d9273110efacca9e67e1cb4fb75238d759e3
Jihiko4450 <anna.demyanenko.4459@mail.ru>
2023-04-06 22:03:50 +0500

    dropdown_menu

--- app/src/main/java/com/example/smartlab/CreateActivity.kt
+++ app/src/main/java/com/example/smartlab/CreateActivity.kt
@@ -1,59 +1,71 @@
 package com.example.smartlab
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.widget.addTextChangedListener
+import com.example.smartlab.databinding.ActivityCreateBinding
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputLayout
 
 class CreateActivity : AppCompatActivity() {
-    lateinit var name:EditText
-    lateinit var lastname:EditText
-    lateinit var patronimic:EditText
-    lateinit var date_of_birth:EditText
-    lateinit var button: MaterialButton
-    lateinit var dropdown:TextInputLayout
+    private lateinit var bind:ActivityCreateBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create)
-        name=findViewById(R.id.editTextTextPersonName)
-        lastname=findViewById(R.id.editTextTextPersonName2)
-        patronimic=findViewById(R.id.editTextTextPersonName3)
-        date_of_birth=findViewById(R.id.editTextTextPersonName4)
-        button=findViewById(R.id.button)
-        dropdown=findViewById(R.id.textInputLayout)
-        val gender=resources.getStringArray(R.array.gender)
-        //val arrayAdapter=ArrayAdapter(requireContext(), R.layout.dropdown_item, gender)
-        name.addTextChangedListener { text ->
-                if (text.toString().length > 0) {
-                    button.isEnabled = true
-                } else {
-                    button.isEnabled = false
-                }
-            }
-        lastname.addTextChangedListener { text ->
-                if (text.toString().length > 0) {
-                    button.isEnabled = true
-                } else {
-                    button.isEnabled = false
-                }
-            }
-        patronimic.addTextChangedListener { text ->
-            if (text.toString().length > 0) {
-                button.isEnabled = true
-            } else {
-                button.isEnabled = false
+        bind=ActivityCreateBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+        val gender =resources.getStringArray(R.array.gender)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, gender)
+        bind.autoCompleteTextView2.setAdapter((arrayAdapter))
+        bind.editTextTextPersonName.addTextChangedListener { text ->
+            if (text.toString().length>0){
+                bind.button.isEnabled = true
+                bind.editTextTextPersonName.setBackgroundResource(R.drawable.selected_input_session2)
+            }else{
+                bind.button.isEnabled = false
+                bind.editTextTextPersonName.setBackgroundResource(R.drawable.selected_input)
             }
         }
-        date_of_birth.addTextChangedListener { text ->
-            if (text.toString().length > 0) {
-                button.isEnabled = true
-            } else {
-                button.isEnabled = false
+        bind.editTextTextPersonName2.addTextChangedListener { text ->
+            if (text.toString().length>0){
+                bind.button.isEnabled = true
+                bind.editTextTextPersonName2.setBackgroundResource(R.drawable.selected_input_session2)
+            }else{
+                bind.button.isEnabled = false
+                bind.editTextTextPersonName2.setBackgroundResource(R.drawable.selected_input)
             }
         }
+        bind.editTextTextPersonName3.addTextChangedListener { text ->
+            if (text.toString().length>0){
+                bind.button.isEnabled = true
+                bind.editTextTextPersonName3.setBackgroundResource(R.drawable.selected_input_session2)
+            }else{
+                bind.button.isEnabled = false
+                bind.editTextTextPersonName3.setBackgroundResource(R.drawable.selected_input)
+            }
+        }
+        bind.editTextTextPersonName4.addTextChangedListener { text ->
+            if (text.toString().length>0){
+                bind.button.isEnabled = true
+                bind.editTextTextPersonName4.setBackgroundResource(R.drawable.selected_input_session2)
+            }else{
+                bind.button.isEnabled = false
+                bind.editTextTextPersonName4.setBackgroundResource(R.drawable.selected_input)
+            }
+        }
+    }
+
+    fun miss4(view: View) {
+        val intent = Intent(this@CreateActivity, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun create(view: View) {
+        val intent = Intent(this@CreateActivity, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
